8d2c2920bf6c18113cb82f33768099760a4e1882
sumbagaara <sumbagaara@gmail.com>
2018-08-09 20:32:29 +0530

    added

---
+++ libMiddleware/lib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib
+{
+    public class Class1
+    {
+		public List<Blog> GetAll()
+		{
+			List<Blog> blogList = new List<Blog>();
+			Blog obj = new Blog();
+			obj.Id = 1;
+			obj.Description = "lol";
+			blogList.Add(obj);
+			return blogList.ToList();
+		}
+    }
+}
